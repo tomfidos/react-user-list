5866fa1c8e7d741ceb8f1c962622f92e67b46ae3
Tomasz Fidos <fidostomasz@gmail.com>
2023-02-23 13:56:53 +0100

    Added user list elements.

---
+++ src/UserList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './UserList.css';
+
+const UserList = () => {
+    return (
+        <ul className="list">
+        </ul>
+    );
+}
+
+export default UserList;
--- src/Users.js
+++ src/Users.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import './Users.css';
+import UserList from './UserList';
 
 class Users extends Component {
 
     render() {
         return (
-            <div className="list">
+            <div className="container">
                 <h1>User list</h1>
                 <form>
                     <input type="text" placeholder="Enter name"/>
                     <button type="submit">Add user</button>
                 </form>
+                <UserList/>
             </div>
         );
     }
